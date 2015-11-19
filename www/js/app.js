1e4a6a02d72e3015ed41a679cd53f24abfc5f0f2
sebastian correa morales <correa.m.sebastian@gmail.com>
2015-11-19 13:17:20 -0500

    ionic add ionic-platform-web-client

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers',
+angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers',
         'starter.services',  'app.auth',
         'app.placas', 'app.fotos', 'app.core', 'app.mfb', 'app.settings', 'app.notificaciones'
     ])
